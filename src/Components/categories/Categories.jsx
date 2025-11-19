7f3eae1e692bcfdae7e13285dc76a6336cb1274c
Amr Zakaria <amr3zakarya@gmail.com>
2025-11-19 19:05:49 +0200

    final

--- src/Components/categories/Categories.jsx
+++ src/Components/categories/Categories.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { PropagateLoader } from "react-spinners";
+import axios from "axios";
+import { data } from "react-router-dom";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  let [loading, setLoading] = useState(true);
+  let [categories, setCategories] = useState({});
+
+  async function getCategories() {
+    return axios
+      .get("https://ecommerce.routemisr.com/api/v1/categories")
+      .then(({ data }) => {
+        setCategories(data);
+        return data;
+      })
+      .catch(({ err }) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  useEffect(() => {
+    if (categories.data) {
+      setLoading(false);
+      console.log(categories);
+    }
+  }, [categories]);
+
+  if (loading) {
+    return (
+      <div className="h-100 d-flex justify-content-center align-items-center mx-auto ">
+        <div className="row gy-4 mt-lg-4">
+          <PropagateLoader color="#0aad0a" />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div className="container w-75 mt-4">
+          <div className="row my-3">
+            {categories.data.map((categoriy) => {
+              return (
+                <div className="col-md-3 bg-light my-4 p-3 text-center">
+                  <img src={categoriy.image} className="w-100" alt="" />
+                  <h3 className="my-2">{categoriy.name}</h3>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </>
+    );
+  }
 };
 
 export default Categories;
